b41786e9b3177c19462058ac66379bebae1adede
Saurav-patel <khiladisaurav59@gmail.com>
2026-03-17 12:00:13 +0530

    understand time&space Complexity

--- Loops.py
+++ Loops.py
@@ -1,14 +1,26 @@
-for i in range(0,5):
-    for j in range(0,5):
-        print("hello from inner loop")
+# for i in range(0,5):
+#     for j in range(0,5):
+#         print("hello from inner loop")
 
 
 
 
 
-for i in range (0,5):
-    # print("+")
-    for j in range(0,i+1):
-        print("*" , end="")
+# for i in range (0,5):
+#     # print("+")
+#     for j in range(0,i+1):
+#         print("*" , end="")
         
-    print()
+#     print()
+
+
+
+# for i in range(0,5):
+#     for j in range(5,i,-1):
+#         print("*" , end="")
+#     print()
+
+
+for i in range(0,5):
+    for j in range(0,i):
+        print("value of i is " , i , "and value of j is " , j)
