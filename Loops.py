acbefc1fc67173dc55770ae25b236bfe284bd1c9
Saurav-patel <khiladisaurav59@gmail.com>
2026-03-17 11:27:45 +0530

    Learn time complexity

---
+++ Loops.py
@@ -0,0 +1,14 @@
+for i in range(0,5):
+    for j in range(0,5):
+        print("hello from inner loop")
+
+
+
+
+
+for i in range (0,5):
+    # print("+")
+    for j in range(0,i+1):
+        print("*" , end="")
+        
+    print()
