edaee1fcb7052a3a4bdbf930473bfdd8b1617495
Saurav-patel <khiladisaurav59@gmail.com>
2026-04-09 12:43:24 +0530

    learning

--- array2.py
+++ array2.py
@@ -42,15 +42,39 @@ def targetSumSubArray(nums , target):
                 
     return max_length
     
-def targetSumSubArrayBetter(nums , target):
+# def targetSumSubArrayBetter(nums , target):
     
-    hash_map = [0] * 20
-    for i in range(len(nums)):
-        hash_map[nums[i]]+=1
+#     hash_map = [0] * 20
+#     for i in range(len(nums)):
+#         hash_map[nums[i]]+=1
+
+#     print(hash_map)
+#     for i in range(len(hash_map)):
+#         summ = 0
+#         maxlength = 0
+#         summ += hash_map[i]
+#         if summ == target:
+#             maxlength = i+1
+    
+#         remaining_sum = target - summ
+#         if remaining_sum >= 0 and remaining_sum < len(hash_map):
+#             length = i - hash_map[remaining_sum] 
+#             maxlength = max(maxlength , length)    
+#     return maxlength
+             
 
 
 
 if __name__ == "__main__":
     arr = [1,1,3,4,5,5,3]
-    print(targetSumSubArray(arr, 5))
-    # print(findNumApperanceOnce(arr))
+    # print(targetSumSubArray(arr, 5))
+    # # print(findNumApperanceOnce(arr))
+    # print(targetSumSubArrayBetter(arr, 5))
+    d = {}
+    target = 5
+    for i,j in enumerate(arr):
+        if target - j in d:
+            print(f"Pair found: ({j}, {target - j})")
+            print(f"Pair found at index from {d[target-j]} to {i}")
+        d[j] = i
+    print(d)
