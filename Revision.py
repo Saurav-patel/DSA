c2c4e734f0b3a9be362237e29001fd718d64a6a7
Saurav-patel <khiladisaurav59@gmail.com>
2026-05-20 11:27:26 +0530

    Enhancing the topics knowledge

--- Revision.py
+++ Revision.py
@@ -6,6 +6,31 @@ def targetSum(arr,target):
             return (mp[diff],i)
         mp[arr[i]] = i
 
+
+
+def substringLongestWithoutRepeating(s):
+    mp = {}
+    start  = 0
+    max_length = 0
+    for end in range(len(s)):
+        if s[end] in mp and mp[s[end]] >= start:
+            start = mp[s[end]] + 1
+            print("start =", start)
+        mp[s[end]] = end
+        max_length = max(max_length, end - start + 1)
+    return max_length
+
+def numAppearMoreThanOnce(arr):
+    mp = {}
+    res = []
+    for i in range(len(arr)):
+        if arr[i] in mp and mp[arr[i]] >= 1:
+            # res.append(arr[i])
+            return arr[i]
+        mp[arr[i]] = mp.get(arr[i],0)+1
+    # return -1
+    return res
+
 if __name__ == "__main__":
     arr = [1,2,3,1,1,2]
 
@@ -15,4 +40,6 @@ if __name__ == "__main__":
 
     # print(freq)
     print(targetSum(arr,4))
-
+    print(substringLongestWithoutRepeating("abcabcbb"))
+    print(substringLongestWithoutRepeating("bcdefg"))
+    print(numAppearMoreThanOnce(arr))
