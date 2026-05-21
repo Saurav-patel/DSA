ad2105761e355f7134599ce50b59e3b0c2fc8aa1
Saurav-patel <khiladisaurav59@gmail.com>
2026-05-21 13:22:07 +0530

    sw + hm

--- Revision.py
+++ Revision.py
@@ -7,6 +7,17 @@ def targetSum(arr,target):
         mp[arr[i]] = i
 
 
+def numAppearMoreThanOnce(arr):
+    mp = {}
+    res = []
+    for i in range(len(arr)):
+        if arr[i] in mp and mp[arr[i]] >= 1:
+            # res.append(arr[i])
+            return arr[i]
+        mp[arr[i]] = mp.get(arr[i],0)+1
+    # return -1
+    return res
+
 
 def substringLongestWithoutRepeating(s):
     mp = {}
@@ -20,16 +31,19 @@ def substringLongestWithoutRepeating(s):
         max_length = max(max_length, end - start + 1)
     return max_length
 
-def numAppearMoreThanOnce(arr):
-    mp = {}
-    res = []
-    for i in range(len(arr)):
-        if arr[i] in mp and mp[arr[i]] >= 1:
-            # res.append(arr[i])
-            return arr[i]
-        mp[arr[i]] = mp.get(arr[i],0)+1
-    # return -1
-    return res
+def minSubArrayWithSumK(arr , target):
+    low = 0
+    
+    summ = 0
+    min_length = float("inf")
+    for high in range(len(arr)):
+        summ += arr[high]
+        while summ >= target:
+            min_length = min(min_length, high - low + 1)
+            summ -= arr[low]
+            low += 1
+    return min_length if min_length != float("inf") else 0
+    
 
 if __name__ == "__main__":
     arr = [1,2,3,1,1,2]
@@ -43,3 +57,4 @@ if __name__ == "__main__":
     print(substringLongestWithoutRepeating("abcabcbb"))
     print(substringLongestWithoutRepeating("bcdefg"))
     print(numAppearMoreThanOnce(arr))
+    print(minSubArrayWithSumK(arr, 7))
